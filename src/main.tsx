755321acf855a22c018c84020ca6d8bf24db553a
NilsGke <n.j.goeke@gmail.com>
2026-04-12 16:08:09 +0200

    change posthog init

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import posthog from "posthog-js";
 import { PostHogProvider } from "@posthog/react";
+
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN, {
+  cookieless_mode: "always",
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: "2026-01-30",
+});
+console.log(posthog);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN}
-      options={{
-        cookieless_mode: "always",
-        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-        defaults: "2026-01-30",
-      }}
-    >
+    <PostHogProvider client={posthog}>
       <App />
     </PostHogProvider>
   </StrictMode>,
